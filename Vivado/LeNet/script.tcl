93abade95b20eb7e7904f08899895dbe7c1811b3
karthisugumar <karthi.sugumar@gmail.com>
2020-03-10 15:50:14 -0700

    Pushed LeNet again

--- Vivado/LeNet/script.tcl
+++ Vivado/LeNet/script.tcl
@@ -5,9 +5,29 @@
 ############################################################
 open_project lenet_cnn
 set_top lenet
+
+#Top
 add_files lenet.cpp
-add_files types.h
+
+#Headers
+add_files weights.h
+add_files bias.h
+add_files convolution.h
+add_files expTable.h
+add_files fc.h
+add_files lenet.h
+add_files pooling.h
+add_files relu.h
+add_files softmax.h
+
 add_files -tb lenet_tb.cpp
+add_files -tb image_0_in.dat
+add_files -tb image_0_out.dat
+add_files -tb image_2_in.dat
+add_files -tb image_2_out.dat
+add_files -tb image_7_in.dat
+add_files -tb image_7_out.dat
+
 open_solution "solution1"
 set_part  {xc7z020clg400-1}
 create_clock -period 10
