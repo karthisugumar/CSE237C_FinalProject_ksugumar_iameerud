ec045862ed977405e27fb80d070f69fb660f90db
karthisugumar <karthi.sugumar@gmail.com>
2020-03-16 02:56:09 -0700

    pushed catapult opt model

---
+++ Catapult/lenet_cnn/directives_127000.tcl
@@ -0,0 +1,278 @@
+//  Catapult HLS Synthesis 10.5/858548 (Beta Release) Sun Feb 16 19:01:19 PST 2020
+//  
+//  Copyright (c) Mentor Graphics Corporation, 1996-2020, All Rights Reserved.
+//                        UNPUBLISHED, LICENSED SOFTWARE.
+//             CONFIDENTIAL AND PROPRIETARY INFORMATION WHICH IS THE
+//                 PROPERTY OF MENTOR GRAPHICS OR ITS LICENSORS
+//  
+//  Running on Linux karthi@Karthi 4.4.0-18362-Microsoft x86_64 aol
+//  
+//  Package information: SIFLIBS v23.5_0.0, HLS_PKGS v23.5_0.0, 
+//                       SIF_TOOLKITS v23.5_0.0, SIF_XILINX v23.5_0.0, 
+//                       SIF_ALTERA v23.5_0.0, CCS_LIBS v23.5_0.0, 
+//                       CDS_PPRO v10.3c_2, CDS_DesigChecker v10.5_beta2, 
+//                       CDS_OASYS v19.1_3.7, CDS_PSR v19.2_0.9, 
+//                       DesignPad v2.78_1.0
+//  
+solution new -state initial
+solution options defaults
+solution options set /Input/CppStandard c++11
+solution options set /Output/GenerateCycleNetlist false
+solution options set /Flows/SCVerify/USE_CCS_BLOCK true
+flow package require /SCVerify
+solution file add ./lenet_tb.cpp -type C++
+directive set -PIPELINE_RAMP_UP true
+directive set -CLUSTER_TYPE combinational
+directive set -CLUSTER_FAST_MODE false
+directive set -CLUSTER_RTL_SYN false
+directive set -CLUSTER_OPT_CONSTANT_INPUTS true
+directive set -CLUSTER_ADDTREE_IN_COUNT_THRESHOLD 0
+directive set -CLUSTER_ADDTREE_IN_WIDTH_THRESHOLD 0
+directive set -ROM_THRESHOLD 64
+directive set -PROTOTYPE_ROM true
+directive set -CHARACTERIZE_ROM false
+directive set -OPT_CONST_MULTS use_library
+directive set -CLOCK_OVERHEAD 20.000000
+directive set -RESET_CLEARS_ALL_REGS use_library
+directive set -START_FLAG {}
+directive set -READY_FLAG {}
+directive set -DONE_FLAG {}
+directive set -TRANSACTION_DONE_SIGNAL true
+directive set -STALL_FLAG false
+directive set -IDLE_SIGNAL {}
+directive set -REGISTER_IDLE_SIGNAL false
+directive set -ARRAY_SIZE 1024
+directive set -CHAN_IO_PROTOCOL use_library
+directive set -IO_MODE super
+directive set -UNROLL no
+directive set -REALLOC true
+directive set -MUXPATH true
+directive set -TIMING_CHECKS true
+directive set -ASSIGN_OVERHEAD 0
+directive set -REGISTER_SHARING_LIMIT 0
+directive set -REGISTER_SHARING_MAX_WIDTH_DIFFERENCE 8
+directive set -SAFE_FSM false
+directive set -NO_X_ASSIGNMENTS true
+directive set -REG_MAX_FANOUT 0
+directive set -FSM_BINARY_ENCODING_THRESHOLD 64
+directive set -FSM_ENCODING none
+directive set -LOGIC_OPT false
+directive set -MEM_MAP_THRESHOLD 32
+directive set -REGISTER_THRESHOLD 256
+directive set -MERGEABLE true
+directive set -SPECULATE true
+directive set -DESIGN_GOAL area
+go new
+solution design set lenet -top
+directive set -ASSUME_ARRAY_INDEX_IN_RANGE true
+directive set -ARRAY_INDEX_OPTIMIZATION true
+directive set -SCHED_USE_MULTICYCLE true
+go analyze
+solution design set lenet -top
+solution library add nangate-45nm_beh -- -rtlsyntool OasysRTL -vendor Nangate -technology 045nm
+solution library add ccs_sample_mem
+solution library add amba
+solution library add {[Block] lenet.v1}
+solution library add Catapult.ROM_1i10_1o16_85e743a9b11d9437f534ac2789164622b5.v1
+solution library add Catapult.ROM_1i10_1o16_a7cfdb78419d9d8086516f4fffa76905b5.v1
+solution library add Catapult.ROM_1i11_1o16_ef4060bd0b8064943dbbe7bfb0448505b5.v1
+solution library add Catapult.ROM_1i12_1o16_5bd22be6a80518328ea853110de5dcf9b5.v1
+solution library add Catapult.ROM_1i12_1o16_ca23bc4f5177291197734bcf05486dabb5.v1
+solution library add Catapult.ROM_1i12_1o16_e0a3a00b8c2f96d90da0c07246b714f5b5.v1
+solution library add Catapult.ROM_1i14_1o16_0744f5169a2333cddc4975cce6ac2e13b6.v1
+solution library add Catapult.ROM_1i14_1o16_e524e57d17518f2513e4a02c680416fcb6.v1
+solution library add Catapult.ROM_1i14_1o16_f622d7671daec8ecc5c08c0f9cc50043b6.v1
+solution library add Catapult.ROM_1i14_1o16_fdab3a38b7cee3cb37a1e480a5a4f171b6.v1
+solution library add Catapult.ROM_1i16_1o15_34b0eb82d8c7b0cef9179780401ecda7b6.v1
+solution library add Catapult.ROM_1i16_1o15_528395d60a8a48b9cebdb555e16904d7b6.v1
+solution library add Catapult.ROM_1i16_1o15_bcbeaff2e050642c7109cf2200fe58d3b6.v1
+solution library add Catapult.ROM_1i16_1o15_c6e6f79bb203b3d071f359e141aa620bb6.v1
+solution library add Catapult.ROM_1i16_1o15_cd00a3bf3491d9ffa0361bddc3cb4956b6.v1
+solution library add Catapult.ROM_1i16_1o15_cf67600c546de637ca3cab4581b793c4b6.v1
+solution library add Catapult.ROM_1i16_1o15_d772f111e00b902569e2532d39a1e9aab6.v1
+solution library add Catapult.ROM_1i16_1o15_ed9e002fc09dd30d9853193cfc271c0bb6.v1
+solution library add Catapult.ROM_1i2_1o12_f70b2b79448202da9e9ef3a94ee0c314b2.v1
+solution library add Catapult.ROM_1i2_1o1_73038c37b1752a032dce5cafbde97746b1.v1
+solution library add Catapult.ROM_1i2_1o1_774e9029769340f390d368ccebeaeef7b1.v1
+solution library add Catapult.ROM_1i2_1o1_ba2465eef8b5f0ecf4d706dbcfcd6f46b1.v1
+solution library add Catapult.ROM_1i2_1o2_211aae8f17da5547546bcf1f716fca0ab1.v1
+solution library add Catapult.ROM_1i2_1o2_262d4ef0e6aefb90fb24c3731f3db051b1.v1
+solution library add Catapult.ROM_1i2_1o2_3601d8c84aadeb22f2dcb08689be6464b1.v1
+solution library add Catapult.ROM_1i2_1o3_530161925910814f79a8128035dc3535b1.v1
+solution library add Catapult.ROM_1i2_1o3_bd80d309628f8e08d0e9e8769f913d35b1.v1
+solution library add Catapult.ROM_1i2_1o4_4a87081d4a8aadb29374fb9dc2700dddb1.v1
+solution library add Catapult.ROM_1i2_1o4_bf8610816c7ad399c7236da87473cafab1.v1
+solution library add Catapult.ROM_1i2_1o5_5fab3ed25ef1c1ad8b2ff44393e6b07db1.v1
+solution library add Catapult.ROM_1i3_1o16_388390bebc76d326bd31e06ab04f7fa7b2.v1
+solution library add Catapult.ROM_1i3_1o16_393c14a69d58689ab24165ae4a275bafb2.v1
+solution library add Catapult.ROM_1i3_1o16_671bd67379646c18b377dbccc24484e1b2.v1
+solution library add Catapult.ROM_1i3_1o16_df6eebf72928902b0d5dc603f522f689b2.v1
+solution library add Catapult.ROM_1i3_1o17_1427f212b24b6fe0f6da4e4b9756d785b2.v1
+solution library add Catapult.ROM_1i3_1o17_1ad872b7b3a3c8a9da48a9eedb3b1196b2.v1
+solution library add Catapult.ROM_1i3_1o17_25be42755e9eb3e057f6a365fc3565bbb2.v1
+solution library add Catapult.ROM_1i3_1o17_37a88a98053c689e76b444b369cf126fb2.v1
+solution library add Catapult.ROM_1i3_1o17_487c1dcbfa4453a829641cdcdcb9501fb2.v1
+solution library add Catapult.ROM_1i3_1o17_816da506b6b61892baf41c4bdd63d97bb2.v1
+solution library add Catapult.ROM_1i3_1o17_92d1aef64a9a70f865d55f7d83cfc617b2.v1
+solution library add Catapult.ROM_1i3_1o17_932c3f41a6eab25175f566563a53bd18b2.v1
+solution library add Catapult.ROM_1i3_1o17_bd892daea1999d74258952707b3bdc28b3.v1
+solution library add Catapult.ROM_1i3_1o17_c16725908d0995a041201e909977e64cb2.v1
+solution library add Catapult.ROM_1i3_1o17_cd6847684e184d4b54a25e219e175553b2.v1
+solution library add Catapult.ROM_1i3_1o17_cdaae1849e21a80ad64262adc9e57b9bb2.v1
+solution library add Catapult.ROM_1i3_1o17_e5921bab7e0422bbb350bb77e1611d4cb2.v1
+solution library add Catapult.ROM_1i3_1o17_f1afef61b9b3ea5f25234b8f5eb0b4f8b3.v1
+solution library add Catapult.ROM_1i4_1o12_078f6f6aa94fb1071c3d0002d02a2a0eb2.v1
+solution library add Catapult.ROM_1i4_1o12_09e237852e96be29d28f6a0d4899f970b2.v1
+solution library add Catapult.ROM_1i4_1o12_5bbd6c901e2ee920412e22f2332d5c6eb2.v1
+solution library add Catapult.ROM_1i4_1o12_8e6c3e2a2359b169b5995607ad05ea96b2.v1
+solution library add Catapult.ROM_1i4_1o12_a4dd0740a6eb00465f77c9c497809001b2.v1
+solution library add Catapult.ROM_1i4_1o12_c206fe985a84fc8f728ec947debd02ffb2.v1
+solution library add Catapult.ROM_1i4_1o13_6e6ddb68b56f96cc40b8b0f6b84ce600b2.v1
+solution library add Catapult.ROM_1i4_1o13_d6c2ea285376f2fe13c888fc85b343c7b2.v1
+solution library add Catapult.ROM_1i5_1o12_58c84a17d0ffb3edff6aca4bec3b63ebb2.v1
+solution library add Catapult.ROM_1i5_1o12_7b356ca21a9cc2cd519bfe31f0e89141b2.v1
+solution library add Catapult.ROM_1i5_1o13_4837190924a4793c8c78eecd1cd7c658b2.v1
+solution library add Catapult.ROM_1i5_1o13_9375ae5dd2fc005f68a6cd0f3d148773b2.v1
+solution library add Catapult.ROM_1i6_1o17_2a0781fff92d31bcb03ed808b8eb867ab3.v1
+solution library add Catapult.ROM_1i6_1o17_a0ece32a1501af193045c96052385c85b3.v1
+solution library add Catapult.ROM_1i7_1o16_005723068140f22a82c6c2a77480e6e0b3.v1
+solution library add Catapult.ROM_1i7_1o16_00724ea599021f4de682be01fe6f8e5fb3.v1
+solution library add Catapult.ROM_1i7_1o16_04cd221e250ef502f6a62e38dd43913db3.v1
+solution library add Catapult.ROM_1i7_1o16_0750a4585c41e281c1b5be20bfb0dcb5b3.v1
+solution library add Catapult.ROM_1i7_1o16_0790f23b6d5fc47bf3946b42cd9a2470b3.v1
+solution library add Catapult.ROM_1i7_1o16_0be05b80f377bf55ce78333b705ab1c8b3.v1
+solution library add Catapult.ROM_1i7_1o16_0dd9d6648e5d04bf6aaaf409a3a2a0b0b3.v1
+solution library add Catapult.ROM_1i7_1o16_0f37f38d3f21ec73f1bac4c873a09f68b3.v1
+solution library add Catapult.ROM_1i7_1o16_0f69c7fe60d4e1edf343f149b13c3dc5b3.v1
+solution library add Catapult.ROM_1i7_1o16_0f73bbf2028d01812c899b536aae8bbeb3.v1
+solution library add Catapult.ROM_1i7_1o16_15639f49c46886b2361a20e3614b991db3.v1
+solution library add Catapult.ROM_1i7_1o16_15adb4e55f9f20d1aa8fa07c252f39afb3.v1
+solution library add Catapult.ROM_1i7_1o16_19d6aca3cd70e4a3df14526632f25217b3.v1
+solution library add Catapult.ROM_1i7_1o16_1c40e2dbf5a9d6442626aa92a75ba34bb3.v1
+solution library add Catapult.ROM_1i7_1o16_1ecc67ddb8ebcada328b0e7fc5d4a2cbb3.v1
+solution library add Catapult.ROM_1i7_1o16_1ef79e626cfad24ad47752c23cdbb04cb3.v1
+solution library add Catapult.ROM_1i7_1o16_20dc2d874eece6d8fbed63382bb3e770b3.v1
+solution library add Catapult.ROM_1i7_1o16_20fa904af118abae1ba4f107052901d2b3.v1
+solution library add Catapult.ROM_1i7_1o16_22f91449bacbbc2600b752189f29a193b3.v1
+solution library add Catapult.ROM_1i7_1o16_2556c9b3333ded0b7e37a164095aa3acb3.v1
+solution library add Catapult.ROM_1i7_1o16_264e9a21477aa593e0840666b0eeafefb3.v1
+solution library add Catapult.ROM_1i7_1o16_2803895cf9809314f813c187a82eab59b3.v1
+solution library add Catapult.ROM_1i7_1o16_284c3d12ef23a85d6325b80a8992026bb3.v1
+solution library add Catapult.ROM_1i7_1o16_2b2e480a9473075c02d832ced49e1e18b3.v1
+solution library add Catapult.ROM_1i7_1o16_2b3085e3f83bdc32c1f4ce22cbbe37fdb3.v1
+solution library add Catapult.ROM_1i7_1o16_34b33115ce6ea7f6674f360f3ef31f8eb3.v1
+solution library add Catapult.ROM_1i7_1o16_34c7510ebc52ba86b3ed77e6c76b138ab3.v1
+solution library add Catapult.ROM_1i7_1o16_36686bb6c08ea8517509d9909c55653cb3.v1
+solution library add Catapult.ROM_1i7_1o16_383cc8bf3d79d6a93459f7678357bf04b3.v1
+solution library add Catapult.ROM_1i7_1o16_3b10eca9f5e6d71f152c4dd78efc984db3.v1
+solution library add Catapult.ROM_1i7_1o16_3c120752647bbef3feeb4a1072df86efb3.v1
+solution library add Catapult.ROM_1i7_1o16_3e7442c2075228baed74318346ee6235b3.v1
+solution library add Catapult.ROM_1i7_1o16_4f6fa3da7b1148951588b970272aeac7b3.v1
+solution library add Catapult.ROM_1i7_1o16_4fd87ac0942ff0376d809c4d270e1293b3.v1
+solution library add Catapult.ROM_1i7_1o16_55dff3d83fc72da9ea0bfb3fb424a238b3.v1
+solution library add Catapult.ROM_1i7_1o16_5a328e69c96610815c29026884f680a8b3.v1
+solution library add Catapult.ROM_1i7_1o16_5b2a83c6b051580962b56fff058775ddb3.v1
+solution library add Catapult.ROM_1i7_1o16_5d37e3fbf3cf29b30b91de3d911f2fb4b3.v1
+solution library add Catapult.ROM_1i7_1o16_608154a7e832112dbc3a78e09c93e67cb3.v1
+solution library add Catapult.ROM_1i7_1o16_6669a016096e28ee8a3f6f584c7b1e9eb3.v1
+solution library add Catapult.ROM_1i7_1o16_68f666f3bb4697d78105a13dac93c396b3.v1
+solution library add Catapult.ROM_1i7_1o16_69aa3c5582b4efe2774dc4cf97e160c2b3.v1
+solution library add Catapult.ROM_1i7_1o16_7b7b8a47ecb078be152aab09ccbd6b32b3.v1
+solution library add Catapult.ROM_1i7_1o16_7c628edc3fc97f1e1314f43872cf3295b3.v1
+solution library add Catapult.ROM_1i7_1o16_81c72049d43b5cc62323d2f4d077ab91b3.v1
+solution library add Catapult.ROM_1i7_1o16_8a2ff726158ab58bf768018eedd5168bb3.v1
+solution library add Catapult.ROM_1i7_1o16_92428d738078c2941cc5dcb56e62d520b3.v1
+solution library add Catapult.ROM_1i7_1o16_935b4b314cfe0d46b36d919b584317ddb3.v1
+solution library add Catapult.ROM_1i7_1o16_93eb83784d9a3a8b842e55962e66e5d7b3.v1
+solution library add Catapult.ROM_1i7_1o16_94db38961dc4868c0a93be76ec4418dbb3.v1
+solution library add Catapult.ROM_1i7_1o16_95b65d37950fc9275bd0ece7d5362e57b3.v1
+solution library add Catapult.ROM_1i7_1o16_95dd7030d47b35ae30074ae66ec17daab3.v1
+solution library add Catapult.ROM_1i7_1o16_9647b650caac3c1e866891ae6fc6d138b3.v1
+solution library add Catapult.ROM_1i7_1o16_97c96c661d69ab751ae8d63b87496507b3.v1
+solution library add Catapult.ROM_1i7_1o16_98dae1c2df06a86ae86fe492975128f3b3.v1
+solution library add Catapult.ROM_1i7_1o16_9a8a465a5d9bf39d7aadd0020ad771ecb3.v1
+solution library add Catapult.ROM_1i7_1o16_9d540b6f9c8b9ed4810180963d24856cb3.v1
+solution library add Catapult.ROM_1i7_1o16_9e0d630dcdb2652ce2f05eeae01a5a54b3.v1
+solution library add Catapult.ROM_1i7_1o16_a40247a864b8fad6d4e186d882bf07fab3.v1
+solution library add Catapult.ROM_1i7_1o16_a93fcb20aa16f15178c9aae9a10f2194b3.v1
+solution library add Catapult.ROM_1i7_1o16_af59cd4543fc8f595d4a5a2a40fc878db3.v1
+solution library add Catapult.ROM_1i7_1o16_b00d618a5740ab12841dfc46c195b4fab3.v1
+solution library add Catapult.ROM_1i7_1o16_b62a035610f87e200382de298005f909b3.v1
+solution library add Catapult.ROM_1i7_1o16_b815c57a50ecb6f40880116d90bcf84bb3.v1
+solution library add Catapult.ROM_1i7_1o16_b83c311acf86f42be1ab7bc50c795c64b3.v1
+solution library add Catapult.ROM_1i7_1o16_b96959c4c0b3b8e38ddb7408b4eb4be6b3.v1
+solution library add Catapult.ROM_1i7_1o16_bbc7667c6e8a77439081af0b45942d30b3.v1
+solution library add Catapult.ROM_1i7_1o16_be0c25dc6f96d2418d3a1b5ad8799637b3.v1
+solution library add Catapult.ROM_1i7_1o16_c15816064da1eda878d34666167eacb1b3.v1
+solution library add Catapult.ROM_1i7_1o16_c39fb2c523d1ddb6a9e60e6d8c47a5edb3.v1
+solution library add Catapult.ROM_1i7_1o16_c524d662df1e5f72aebe62ed2aac93dab3.v1
+solution library add Catapult.ROM_1i7_1o16_c9aa8d25e1dc73e4a4f22e9c74bfa957b3.v1
+solution library add Catapult.ROM_1i7_1o16_ca6e62747d002461a6ca5e053ca78febb3.v1
+solution library add Catapult.ROM_1i7_1o16_cb22c1bbee9abf3ca391f6a08fcfdd1eb3.v1
+solution library add Catapult.ROM_1i7_1o16_cf20d5e775ee40d1904eedc0f5b3c42cb3.v1
+solution library add Catapult.ROM_1i7_1o16_cf2608b546cf1b9dc5e1120e9aa8ddc4b3.v1
+solution library add Catapult.ROM_1i7_1o16_cfbe48f4db15010cbbfb2901ee5eed5fb3.v1
+solution library add Catapult.ROM_1i7_1o16_d1acd7a2ab1492a583f43f2a426cc874b3.v1
+solution library add Catapult.ROM_1i7_1o16_d36b7b85f3e276ac8b075d6d90d02bdcb3.v1
+solution library add Catapult.ROM_1i7_1o16_d3ec4d54983225a96396d995c0c44306b3.v1
+solution library add Catapult.ROM_1i7_1o16_d50c61639e329278f7c69f04c0d028f6b3.v1
+solution library add Catapult.ROM_1i7_1o16_d5990b32e585b2fa59d5323273ecbce8b3.v1
+solution library add Catapult.ROM_1i7_1o16_db5001e37878aca8b46e487472cdbdd7b3.v1
+solution library add Catapult.ROM_1i7_1o16_df26579a55c5f5821630b21487c3695bb3.v1
+solution library add Catapult.ROM_1i7_1o16_dff9108ce573b2c7150443723a31f594b3.v1
+solution library add Catapult.ROM_1i7_1o16_e2d4e7d0a2b482941b8be59f681a1239b3.v1
+solution library add Catapult.ROM_1i7_1o16_e83d3b92ec7dd32df8a1036852009b47b3.v1
+solution library add Catapult.ROM_1i7_1o16_e919b2fc5fdf379aaae864e42a514c32b3.v1
+solution library add Catapult.ROM_1i7_1o16_f0dc514144a76adf47d5391ba9603e34b3.v1
+solution library add Catapult.ROM_1i7_1o16_f224c1af8ff0df4118fddc37658a970fb3.v1
+solution library add Catapult.ROM_1i7_1o16_f3b7adece84e6f65ad1e03f16c27697ab3.v1
+solution library add Catapult.ROM_1i7_1o16_f77c10022bdb3989613bde4d8414a77fb3.v1
+solution library add Catapult.ROM_1i7_1o16_f9644026b5c8e8b564b5518505697eafb3.v1
+solution library add Catapult.ROM_1i7_1o16_fa0eb3b907288e6c081e4d0a9960f6cfb3.v1
+solution library add Catapult.ROM_1i7_1o16_fa29de6434d65a5e292d7622cf31f34bb3.v1
+solution library add Catapult.ROM_1i7_1o16_fc654bcd1af5c09b9d7749901b0966d4b3.v1
+solution library add Catapult.ROM_1i7_1o16_fd2bc0395a5a96dcf86dc4817a104f28b3.v1
+solution library add Catapult.ROM_1i7_1o16_fe3a4fdbdc50ff399ebd63e49f3624cfb3.v1
+solution library add Catapult.ROM_1i7_1o16_fe68e4310ec62fbde212636f95e6c61db3.v1
+solution library add Catapult.ROM_1i7_1o16_fef72c4157e27c691f58452ee1f2091eb3.v1
+solution library add Catapult.ROM_1i9_1o32_9f40a3e264d30a3e1389a72cec4d39adb3.v1
+go libraries
+directive set -CLOCKS {clk {-CLOCK_PERIOD 2.0 -CLOCK_EDGE rising -CLOCK_UNCERTAINTY 0.0 -CLOCK_HIGH_TIME 1.0 -RESET_SYNC_NAME rst -RESET_ASYNC_NAME arst_n -RESET_KIND sync -RESET_SYNC_ACTIVE high -RESET_ASYNC_ACTIVE low -ENABLE_ACTIVE high}}
+go assembly
+directive set /lenet/core/K_COL#3 -UNROLL yes
+directive set /lenet/core/K_ROW#3 -UNROLL yes
+directive set /lenet/core/K_COL#2 -UNROLL yes
+directive set /lenet/core/K_ROW#2 -UNROLL yes
+directive set /lenet/core/K_COL#1 -UNROLL yes
+directive set /lenet/core/K_ROW#1 -UNROLL yes
+directive set /lenet/core/K_COL -UNROLL yes
+directive set /lenet/core/K_ROW -UNROLL yes
+directive set /lenet/core/R3_OUT -PIPELINE_INIT_INTERVAL 1
+directive set /lenet/core/R4_OUT -PIPELINE_INIT_INTERVAL 1
+directive set /lenet/core/C1_COL_CPY -PIPELINE_INIT_INTERVAL 0
+directive set /lenet/C1_weights.rom:rsc -INTERLEAVE 0
+directive set /lenet/C1_biases.rom:rsc -INTERLEAVE 0
+directive set /lenet/core/C1_out:rsc -INTERLEAVE 0
+directive set /lenet/core/C1_out:rsc -BLOCK_SIZE 0
+directive set /lenet/C1_weights.rom:rsc -BLOCK_SIZE 0
+directive set /lenet/C1_biases.rom:rsc -BLOCK_SIZE 0
+directive set /lenet/image_in:rsc -BLOCK_SIZE 0
+directive set /lenet/core/C1_COL_CLR -PIPELINE_INIT_INTERVAL 0
+directive set /lenet/core/P1_COL -PIPELINE_INIT_INTERVAL 0
+directive set /lenet/core/P2_COL -PIPELINE_INIT_INTERVAL 0
+directive set /lenet/core/C1_COL -PIPELINE_INIT_INTERVAL 0
+directive set /lenet/core/C2_COL -PIPELINE_INIT_INTERVAL 0
+directive set /lenet/core/F_ROW -PIPELINE_INIT_INTERVAL 2
+directive set /lenet/core/OUT -PIPELINE_INIT_INTERVAL 1
+directive set /lenet/core/R1_ROW -PIPELINE_INIT_INTERVAL 1
+directive set /lenet/core/R2_ROW -PIPELINE_INIT_INTERVAL 1
+directive set /lenet/core/DEN -PIPELINE_INIT_INTERVAL 1
+directive set /lenet/core/FC1_IN -PIPELINE_INIT_INTERVAL 10
+directive set /lenet/core/FC2_IN -PIPELINE_INIT_INTERVAL 10
+directive set /lenet/core/FC3_IN -PIPELINE_INIT_INTERVAL 10
+directive set /lenet/core/FC1_out:rsc -INTERLEAVE 8
+directive set /lenet/FC1_bias.rom:rsc -INTERLEAVE 0
+directive set /lenet/FC1_weights.rom:rsc -INTERLEAVE 0
+directive set /lenet/core/FC2_out:rsc -INTERLEAVE 4
+directive set /lenet/core/FC3_out:rsc -INTERLEAVE 2
+go assembly
---
+++ Catapult/lenet_cnn/lenet_tb.cpp
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include<fstream>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+#include "lenet.h"
+#include "lenet_cnn.h"
+
+struct Rmse
+{
+    int num_sq;
+    float sum_sq;
+    float error;
+    
+    Rmse(){ num_sq = 0; sum_sq = 0; error = 0; }
+    
+    float add_value(float d_n)
+    {
+        num_sq++;
+        sum_sq += (d_n*d_n);
+        error = sqrtf(sum_sq / num_sq);
+        return error;
+    }
+    
+};
+
+Rmse rmse;
+
+int main()
+{
+    //int r,c,s;
+    float temp;
+    float gold;
+    FILE *fp;
+
+  //  DTYPE filt[NUM_OUTCHAN][NUM_INCHAN][KSIZE][KSIZE];
+    DTYPE img[C1_NUM_INCHAN][C1_IN_ROWS][C1_IN_COLS];
+    DTYPE out[PREDS];
+ //   DTYPE bias[NUM_OUTCHAN];
+
+    fp = fopen("image_0_in.dat", "r");
+    for(int ifm=0; ifm < C1_NUM_INCHAN; ifm++) {
+		for(int r=0; r < C1_IN_ROWS; r++) {
+			for(int c=0; c < C1_IN_COLS; c++) {
+				fscanf(fp, "%f\n", &temp);
+			img[ifm][r][c] = temp;
+			}
+		}
+    }
+    fclose(fp);
+
+
+//    fp = fopen("conv1_kernel.dat", "r");
+//    for(int ofm=0; ofm < NUM_OUTCHAN; ofm++) {
+//    	for(int ifm=0; ifm < NUM_INCHAN; ifm++) {
+//		for(int r=0; r < KSIZE; r++) {
+//			for(int c=0; c < KSIZE; c++) {
+//				fscanf(fp, "%f\n", &temp);
+//			filt[ofm][ifm][r][c] = temp;
+//			}
+//		}
+//    	}
+//    }
+//    fclose(fp);
+
+
+//    fp = fopen("conv1_bias.dat", "r");
+//    for(int r=0; r < NUM_OUTCHAN; r++) {
+//        fscanf(fp, "%f\n", &temp);
+//        bias[r] = temp;
+//    }
+//    fclose(fp);
+
+   // filt = C1_weights;
+
+    lenet(img, out);
+
+    // fp = fopen("conv1_out.dat", "r");
+    // for(int ofm=0; ofm < NUM_OUTCHAN; ofm++) {
+    //     for(int r=0; r < OUT_ROWS; r++) {
+    //         for(int c=0; c < OUT_COLS; c++) {
+    //             fscanf(fp, "%f\n", &temp);
+    // //          cout << "OUT: " << out[ofm][r][c] << endl;
+    //         rmse.add_value(out[ofm][r][c] - temp);
+    //         }
+    //     }
+    // }
+    // fclose(fp);
+
+    fp = fopen("image_0_out.dat", "r");
+    for(int p=0; p < PREDS; p++) {
+		fscanf(fp, "%f\n", &temp);
+		cout << "PREDS: " << out[p] << endl;
+	   //rmse.add_value((float)(out[p]) - temp);
+    }
+    fclose(fp);
+
+    // printing error results
+    printf("----------------------------------------------\n");
+    printf("   RMSE ");
+    printf("%f\n", rmse.error);
+    printf("----------------------------------------------\n");
+
+    if (rmse.error > 0.001) {
+        fprintf(stdout, "*******************************************\n");
+        fprintf(stdout, "FAIL: Output DOES NOT match the golden output\n");
+        fprintf(stdout, "*******************************************\n");
+        return 1;
+    }else {
+        fprintf(stdout, "*******************************************\n");
+        fprintf(stdout, "PASS: The output matches the golden output!\n");
+        fprintf(stdout, "*******************************************\n");
+        return 0;
+    }
+    return 0;
+}
